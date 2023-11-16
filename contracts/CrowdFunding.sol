58967c8081b147e367ee84c662dc3edfe409c43c
Jagjot Singh <dev.flenex@gmail.com>
2023-11-16 22:20:07 +0530

    Updated CrowdFunding.sol

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,25 +2,22 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract CrowdFunding {
-    struct Contributor {
+    struct Contributers {
         string name;
         uint256 timestamp;
         address from;
     }
 
-    Contributor[] public contributors;
-    address payable public owner =
-        payable(0xF3bA650f3BB16dc176aa30fa2EB07Ec0cb7E1B26);
+    Contributers[] contributers;
+    address payable owner = payable(0xF3bA650f3BB16dc176aa30fa2EB07Ec0cb7E1B26);
 
     function sendDonation(string memory name) public payable {
-        require(msg.value > 0 ether, "Please pay greater than 0 ether");
-        require(bytes(name).length > 0, "Please provide your name");
-
+        require(msg.value > 0, "Please pay greater than 0 ether");
         owner.transfer(msg.value);
-        contributors.push(Contributor(name, block.timestamp, msg.sender));
+        contributers.push(Contributers(name, block.timestamp, msg.sender));
     }
 
-    function getContributors() public view returns (Contributor[] memory) {
-        return contributors;
+    function getContributers() public view returns (Contributers[] memory) {
+        return contributers;
     }
 }
